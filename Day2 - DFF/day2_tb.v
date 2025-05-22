ebfbdc809d409c4e9f6fea1027c6e2425ce8b6e7
RauwP <147203549+RauwP@users.noreply.github.com>
2025-05-22 10:38:18 +0300

    Day 2 - creating DFF with async/sync reset
    Good demonstrations of what lint errors are and how to use always_ff.

---
+++ Day2 - DFF/day2_tb.v
@@ -0,0 +1,38 @@
+// DFF TB
+
+module day2_tb ();
+	logic clk;
+  logic reset;
+  logic d_i;
+  logic q_norst_o;
+  logic q_syncrst_o;
+  logic q_asyncrst_o;
+  
+  day2 DAY2(.*);
+  always begin
+    clk = 1'b1;
+    #5;
+    clk = 1'b0;
+    #5;
+  end
+    
+  initial begin
+    reset <= 1'b1;
+    d_i <= 1'b0;
+    @(posedge clk)
+    reset <= 1'b0;
+    @(posedge clk)
+    d_i <= 1'b1;
+    @(posedge clk)
+    @(posedge clk)
+    @(posedge clk)
+    reset <= 1'b1;
+    @(posedge clk)
+    @(posedge clk)
+    reset <= 1'b0;
+    @(posedge clk)
+    @(posedge clk)
+    $finish();
+  end
+
+endmodule
