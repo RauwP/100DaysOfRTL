06bbdba51c23e65e1f5848d1bb9ab739fa5b3e92
Gad B. <rauwp@duck.com>
2025-08-17 23:16:46 +0300

    not day 069

--- Day069-UVM_TB_For_APB_Slave_8/apb_intf.sv
+++
@@ -1,8 +0,0 @@
-interface apb_slave_if(
-    input       logic       clk,
-    input       logic       reset
-);
-logic psel, penable, pwrite, pready;
-logic[31:0] pwdata, prdata;
-logic[9:0] paddr;
-endinterface
